c64d32a16082144f9a7dffa5bba2605432b33bc1
Luis Ron <lcabeza@hmoore.com.ar>
2023-11-14 12:02:42 -0300

    logica de boton pagar

---
+++ Pages/Pedido/Pago.Realizado.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pago</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+
+<body>
+
+    <!-- Titulo -->
+    <div class="px-4 my-5 text-center">
+        <h1 class="display-5 fw-bold text-body-emphasis">Pago Realizado</h1>
+    </div>
+
+
+    <div class="container text-center">
+        <img src="../../public/coco.png" class="px-4 text-center mb-4 img-fluid " alt="Success" width="500"
+            loading="lazy">
+    </div>
+
+
+    <div class="text-center mb-3">
+        <a href="../Categorias/Categoria.php" class="btn btn-primary mx-2">Realizar Otra Compra</a>
+        <a href="../../index.php" class="btn btn-success mx-2">Volver a Home</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
