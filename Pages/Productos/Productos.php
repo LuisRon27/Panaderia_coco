c97a57f58010b11e24ba32000e7c898bca76940c
Jesus <Jesusan0110@gmail.com>
2023-11-17 19:02:34 -0300

    chore:stilyng-index

--- Pages/Productos/Productos.php
+++ Pages/Productos/Productos.php
@@ -39,16 +39,16 @@
 
         <main>
 
-            <div class="container mt-4">
-                <div class="row justify-content-center">
-                    <div class="col-md-9">
-                        <div class="input-group mb-3">
+            <div class="container-fluid mt-4">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-10 d-flex justify-content-center ms-4">
+                        <div class="input-group mb-3 w-50">
                             <input type="text" id="busqueda" class="form-control" placeholder="Buscar productos" aria-label="Buscar productos" aria-describedby="button-buscar">
                             <button class="btn btn-primary" id="button-buscar" onclick="filtrarProductos()">Buscar</button>
                         </div>
 
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-1 justify-content-end">
                         <?php include("../Carrito/Carrito.php"); ?>
                     </div>
 
@@ -72,82 +72,82 @@
             ?>
 
                 <!-- Card Producto -->
-                <div class="d-flex">
-                    <div class="row d-flex justify-content-center">
-                        <div class="d-inline-flex  justify-content-center col-md-4 mb-3">
-                            <div class="card " style="width: 18rem;">
-                                <img src="<?php echo $fila['ImagenURL'] ?>" class="card-img-top" alt="<?php echo $fila['Nombre'] ?>">
-                                <div class="card-body">
-                                    <h5 class="card-title">
-                                        <?php echo $fila['Nombre'] ?>
-                                    </h5>
-                                    <p class="card-text">
-                                        <?php echo $fila['Descripcion'] ?>
-                                    </p>
 
-                                    <!-- Button trigger modal -->
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $fila['IDProducto'] ?>">
-                                        Comprar
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="d-inline-flex  justify-content-center col-md-4 mb-3">
+                        <div class="card " style="width: 18rem;">
+                            <img src="<?php echo $fila['ImagenURL'] ?>" class="card-img-top" alt="<?php echo $fila['Nombre'] ?>">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?php echo $fila['Nombre'] ?>
+                                </h5>
+                                <p class="card-text">
+                                    <?php echo $fila['Descripcion'] ?>
+                                </p>
 
-
-
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal<?php echo $fila['IDProducto'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?php echo $fila['IDProducto'] ?>" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <form action="../Carrito/ABM/Insertar.Carrito.php" method="post">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel<?php echo $fila['IDProducto'] ?>">
-                                            <?php echo $fila['Nombre'] ?>
-                                        </h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img src="<?php echo $fila['ImagenURL'] ?>" class="card-img-top" alt="<?php echo $fila['Nombre'] ?>">
-                                        <h4 class="mt-4">
-                                            $ARS
-                                            <?php echo $fila['Precio'] ?>
-                                        </h4>
-                                        <p class="fs-4">
-                                            <?php echo $fila['Descripcion'] ?>
-                                        </p>
-
-                                        <!-- Contador (Cantidad) -->
-                                        <div class="">
-                                            <h4>Cantidad</h4>
-                                            <div class="contador">
-                                                <input type="text" class="numero form-control" name="Cantidad" data-producto-id="<?php echo $fila['IDProducto'] ?>" value="0">
-                                            </div>
-                                            <div class="mt-3">
-                                                <a class="btn btn-primary" onclick="incrementarContador(<?php echo $fila['IDProducto'] ?>)">+</a>
-                                                <a class="btn btn-primary" onclick="decrementarContador(<?php echo $fila['IDProducto'] ?>)">-</a>
-                                            </div>
-                                        </div>
-
-                                        <!-- Campos Ocultos -->
-                                        <input type="hidden" id="IDUsuario" name="IDUsuario" value="<?php echo $IDUsuario ?>">
-                                        <input type="hidden" id="IDCategoria" name="IDCategoria" value="<?php echo $IDCategoria ?>">
-                                        <input type="hidden" id="IDProducto" name="IDProducto" value="<?php echo $fila['IDProducto'] ?>">
-
-                                    </div>
-                                    <div class="mb-3 p-3">
-                                        <label for="Comentarios" class="form-label">Comentarios</label>
-                                        <textarea class="form-control" id="Comentarios" name="Comentarios" rows="3"></textarea>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-primary">Agregar Al Carrito</button>
-                                    </div>
-                                </form>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $fila['IDProducto'] ?>">
+                                    Comprar
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal<?php echo $fila['IDProducto'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?php echo $fila['IDProducto'] ?>" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <form action="../Carrito/ABM/Insertar.Carrito.php" method="post">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel<?php echo $fila['IDProducto'] ?>">
+                                        <?php echo $fila['Nombre'] ?>
+                                    </h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img src="<?php echo $fila['ImagenURL'] ?>" class="card-img-top" alt="<?php echo $fila['Nombre'] ?>">
+                                    <h4 class="mt-4">
+                                        $ARS
+                                        <?php echo $fila['Precio'] ?>
+                                    </h4>
+                                    <p class="fs-4">
+                                        <?php echo $fila['Descripcion'] ?>
+                                    </p>
+
+                                    <!-- Contador (Cantidad) -->
+                                    <div class="">
+                                        <h4>Cantidad</h4>
+                                        <div class="contador">
+                                            <input type="text" class="numero form-control" name="Cantidad" data-producto-id="<?php echo $fila['IDProducto'] ?>" value="0">
+                                        </div>
+                                        <div class="mt-3">
+                                            <a class="btn btn-primary" onclick="incrementarContador(<?php echo $fila['IDProducto'] ?>)">+</a>
+                                            <a class="btn btn-primary" onclick="decrementarContador(<?php echo $fila['IDProducto'] ?>)">-</a>
+                                        </div>
+                                    </div>
+
+                                    <!-- Campos Ocultos -->
+                                    <input type="hidden" id="IDUsuario" name="IDUsuario" value="<?php echo $IDUsuario ?>">
+                                    <input type="hidden" id="IDCategoria" name="IDCategoria" value="<?php echo $IDCategoria ?>">
+                                    <input type="hidden" id="IDProducto" name="IDProducto" value="<?php echo $fila['IDProducto'] ?>">
+
+                                </div>
+                                <div class="mb-3 p-3">
+                                    <label for="Comentarios" class="form-label">Comentarios</label>
+                                    <textarea class="form-control" id="Comentarios" name="Comentarios" rows="3"></textarea>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Agregar Al Carrito</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
 
             <?php
             }
